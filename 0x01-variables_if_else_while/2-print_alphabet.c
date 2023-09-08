647d6086586fa4ee7697921a6b11cf68c16d026f
MalulekaK <stkmaluleka462@gmail.com>
2023-09-08 14:11:44 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,12 +5,12 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
 	char alphabet = 'a';
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-putchar(alphabet);
-
+	{
+		putchar(alphabet);
+	}
 	return (0);
 }
