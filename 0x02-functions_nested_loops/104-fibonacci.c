0c5e4e6e62ab6749dfad04eba25a548200676ea1
MalulekaK <stkmaluleka462@gmail.com>
2023-09-12 16:58:56 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 98 Fibonacci numbers, starting with
+ * 1 and 2, separated by a comma followed by a space.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+}
