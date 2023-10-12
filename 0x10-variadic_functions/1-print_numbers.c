3b641081e1f9f9a5877e7b89c098409d47a307b3
MalulekaK <stkmaluleka462@gmail.com>
2023-10-12 08:37:05 -0700

    Update

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,20 +13,39 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	char *sep;
+	#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Entry Point
+ * @separator: Print separator if it's not NULL and
+ * not the last number
+ * @n: elements to be printed
+ *
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
 	unsigned int i;
-	va_list list;
 
-	if (separator == NULL || *separator == 0)
-		sep = "";
-	else
-		sep = (char *) separator;
-	va_start(list, n);
+	va_start(args, n);
 
-	if (n > 0)
-		printf("%d", va_arg(list, int));
-	for (i = 1; i < n; i++)
-		printf("%s%d", sep, va_arg(list, int));
+	for (i = 0; i < n; i++)
+	{
+		int num = va_arg(args, int);
+
+		printf("%d", num);
+
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+
+	va_end(args);
+
 	printf("\n");
-	va_end(list);
 }
