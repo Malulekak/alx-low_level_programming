a5301afac16fa241fedb3c91672a25bcace45608
MalulekaK <stkmaluleka462@gmail.com>
2023-10-19 05:19:56 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * printMessage -  prints You're beat! and yet, you must 
+ * allow,\nI bore my house upon my back!\n before 
+ * the main function is executed.
+ *
+ */
+
+void printMessage(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
