62fa8159a6ed617e0e8524ffc80a8452c1e1e0e0
MalulekaK <stkmaluleka462@gmail.com>
2023-10-21 03:52:15 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
+void printMessage(void) __attribute__((constructor));
+
 /**
- * printMessage -  prints You're beat! and yet, you must
- * allow,\nI bore my house upon my back!\n before
- * the main function is executed.
- *
- */
+* printMessage -  prints You're beat! and yet, you must
+* allow,\nI bore my house upon my back!\n before
+* the main function is executed.
+*
+*/
 
 void printMessage(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("%s\n%s\n",
+	"You're beat! and yet, you must allow,",
+	"I bore my house upon my back!"
+	);
 }
